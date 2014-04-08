ee57359be79e739eae7bacb004d489b681f84138
Mark Burnett <mark.m.burnett@gmail.com>
2014-04-08 12:41:03 -0500

    extract method: _create_operation

--- ptero_workflow/implementation/backend.py
+++ ptero_workflow/implementation/backend.py
@@ -10,28 +10,18 @@ class Backend(object):
     def create_workflow(self, workflow_data):
         self._validate_workflow_data(workflow_data)
 
-        workflow_data['operations']['input connector']  = {'type': 'input'}
-        workflow_data['operations']['output connector'] = {'type': 'output'}
-
         workflow = models.Workflow(
             inputs=simplejson.dumps(workflow_data['inputs']),
             environment=simplejson.dumps(workflow_data['environment']),
         )
 
-        workflow.root_operation = models.Operation(name='root', type='root')
+        root_operation_data = {
+            'type': 'model',
+            'operations': workflow_data['operations'],
+            'links': workflow_data['links'],
+        }
 
-        for name, operation_data in workflow_data['operations'].iteritems():
-            workflow.operations[name] = models.Operation(
-                    name=name, **operation_data)
-
-        for link_data in workflow_data['links']:
-            source = workflow.operations[link_data['source']]
-            destination = workflow.operations[link_data['destination']]
-            models.Link(
-                    source_operation=source,
-                    destination_operation=destination,
-                    source_property=link_data['source_property'],
-                    destination_property=link_data['destination_property'])
+        workflow.root_operation = _create_operation(root_operation_data)
 
         self.session.add(workflow)
         self.session.commit()
@@ -52,3 +42,25 @@ class Backend(object):
         if 'output connector' in workflow_data['operations']:
             raise exceptions.InvalidWorkflow(
                     "'output connector' is a reserved operation name")
+
+
+def _create_operation(operation_data):
+    operation_data['operations']['input connector']  = {'type': 'input'}
+    operation_data['operations']['output connector'] = {'type': 'output'}
+
+    root_operation = models.Operation(name='root', type='root')
+
+    for name, child_operation_data in operation_data['operations'].iteritems():
+        root_operation.children[name] = models.Operation(name=name,
+                **child_operation_data)
+
+    for link_data in operation_data['links']:
+        source = root_operation.children[link_data['source']]
+        destination = root_operation.children[link_data['destination']]
+        models.Link(
+                source_operation=source,
+                destination_operation=destination,
+                source_property=link_data['source_property'],
+                destination_property=link_data['destination_property'])
+
+    return root_operation
