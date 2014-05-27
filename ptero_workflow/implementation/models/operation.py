be44d63f2a068c389716339c19688ce9a59cf46f
Mark Burnett <mark.m.burnett@gmail.com>
2014-05-27 08:39:26 -0500

    split get_petri_transitions method

--- ptero_workflow/implementation/models/operation.py
+++ ptero_workflow/implementation/models/operation.py
@@ -91,17 +91,45 @@ class Operation(Base):
         )
 
     def get_petri_transitions(self):
-        result = []
+        transitions = []
 
-        # wait for all input ops
-        result.append({
+        input_deps_place = self._attach_input_deps(transitions)
+
+        split_place = self._attach_split(transitions, input_deps_place)
+        action_place = self._attach_action(transitions, split_place)
+        join_place = self._attach_join(transitions, action_place)
+
+        self._attach_output_deps(transitions, join_place)
+
+        return transitions
+
+
+    def _attach_input_deps(self, transitions):
+        transitions.append({
             'inputs': [o.success_place_pair_name(self) for o in self.input_ops],
             'outputs': [self.ready_place_name],
         })
 
+        return self.ready_place_name
+
+    def _attach_output_deps(self, transitions, internal_success_place):
+        success_outputs = [self.success_place_pair_name(o) for o in self.output_ops]
+        success_outputs.append(self.success_place_pair_name(self.parent))
+        transitions.append({
+            'inputs': [internal_success_place],
+            'outputs': success_outputs,
+        })
+
+    def _attach_split(self, transitions, ready_place):
+        return ready_place
+
+    def _attach_join(self, transitions, action_done_place):
+        return action_done_place
+
+    def _attach_action(self, transitions, action_ready_place):
         # send notification
-        result.append({
-            'inputs': [self.ready_place_name],
+        transitions.append({
+            'inputs': [action_ready_place],
             'outputs': [self.response_wait_place_name],
             'action': {
                 'type': 'notify',
@@ -113,20 +141,14 @@ class Operation(Base):
         })
 
         # wait for response
-        result.append({
+        transitions.append({
             'inputs': [self.response_wait_place_name,
                 self.response_callback_place_name],
             'outputs': [self.success_place_name],
         })
 
-        success_outputs = [self.success_place_pair_name(o) for o in self.output_ops]
-        success_outputs.append(self.success_place_pair_name(self.parent))
-        result.append({
-            'inputs': [self.success_place_name],
-            'outputs': success_outputs,
-        })
+        return self.success_place_name
 
-        return result
 
     @property
     def input_ops(self):
