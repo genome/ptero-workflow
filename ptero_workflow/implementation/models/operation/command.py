a0b8287f05f5588bb718adb6c6399b2ccbd68d16
Mark Burnett <mark.m.burnett@gmail.com>
2014-06-03 08:57:33 -0500

    move code from CommandOperation to command mixin

--- ptero_workflow/implementation/models/operation/command.py
+++ ptero_workflow/implementation/models/operation/command.py
@@ -1,14 +1,10 @@
 from ..base import Base
-from ..job import Job, ResponseLink
 from .operation_base import Operation
 from .mixins.command import OperationPetriMixin
 from .mixins.parallel import ParallelPetriMixin
 from sqlalchemy import Column, ForeignKey, Integer, Text, UniqueConstraint
 from sqlalchemy.orm import backref, relationship
 from sqlalchemy.orm.collections import attribute_mapped_collection
-from sqlalchemy.orm.session import object_session
-import os
-import requests
 import simplejson
 
 
@@ -56,61 +52,3 @@ class CommandOperation(OperationPetriMixin, Operation):
     }
 
     VALID_EVENT_TYPES = Operation.VALID_EVENT_TYPES.union(['execute', 'ended'])
-
-    def execute(self, body_data, query_string_data):
-        color = body_data['color']
-        group = body_data['group']
-        response_links = body_data['response_links']
-
-        method_name = query_string_data['method']
-        method = self.methods[method_name]
-
-        job_id = self._submit_to_fork(color, method.command_line)
-
-        job = Job(operation=self, method=method, color=color, job_id=job_id)
-        s = object_session(self)
-        for name, url in response_links.iteritems():
-            link = ResponseLink(job=job, url=url, name=name)
-            job.response_links[name] = link
-
-        s.add(job)
-        s.commit()
-
-    def ended(self, body_data, query_string_data):
-        job_id = body_data.pop('job_id')
-
-        s = object_session(self)
-        job = s.query(Job).filter_by(operation=self, job_id=job_id).one()
-
-        if body_data['exit_code'] == 0:
-            outputs = simplejson.loads(body_data['stdout'])
-            self.set_outputs(outputs, job.color)
-            s.commit()
-            return requests.put(job.response_links['success'].url)
-
-        else:
-            return requests.put(job.response_links['failure'].url)
-
-    def _submit_to_fork(self, color, command_line):
-        body_data = self._fork_submit_data(color, command_line)
-        response = requests.post(self._fork_submit_url,
-                data=simplejson.dumps(body_data),
-                headers={'Content-Type': 'application/json'})
-        return response.json()['job_id']
-
-    @property
-    def _fork_submit_url(self):
-        return 'http://%s:%d/v1/jobs' % (
-            os.environ.get('PTERO_FORK_HOST', 'localhost'),
-            int(os.environ.get('PTERO_FORK_PORT', 80)),
-        )
-
-    def _fork_submit_data(self, color, command_line):
-        return {
-            'command_line': command_line,
-            'user': os.environ.get('USER'),
-            'stdin': simplejson.dumps(self.get_inputs(color)),
-            'callbacks': {
-                'ended': self.event_url('ended'),
-            },
-        }
