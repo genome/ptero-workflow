8d58c90bed89275345c93436f1799f4fbd26d327
Mark Burnett <mark.m.burnett@gmail.com>
2014-06-03 09:57:40 -0500

    extract base petri mixin

---
+++ ptero_workflow/implementation/models/operation/mixins/base.py
@@ -0,0 +1,38 @@
+class BasePetriMixin(object):
+    def get_petri_transitions(self):
+        transitions = []
+
+        input_deps_place = self._attach_input_deps(transitions)
+
+        split_place = self._attach_split(transitions, input_deps_place)
+        action_place = self._attach_action(transitions, split_place)
+        join_place = self._attach_join(transitions, action_place)
+
+        self._attach_output_deps(transitions, join_place)
+
+        return transitions
+
+    def _attach_input_deps(self, transitions):
+        transitions.append({
+            'inputs': [o.success_place_pair_name(self) for o in self.input_ops],
+            'outputs': [self.ready_place_name],
+        })
+
+        return self.ready_place_name
+
+    def _attach_output_deps(self, transitions, internal_success_place):
+        success_outputs = [self.success_place_pair_name(o) for o in self.output_ops]
+        success_outputs.append(self.success_place_pair_name(self.parent))
+        transitions.append({
+            'inputs': [internal_success_place],
+            'outputs': success_outputs,
+        })
+
+    def _attach_split(self, transitions, ready_place):
+        return ready_place
+
+    def _attach_join(self, transitions, action_done_place):
+        return action_done_place
+
+    def _attach_action(self, transitions, action_ready_place):
+        raise RuntimeError('%s is abstract' % self.__class__.__name__)
--- ptero_workflow/implementation/models/operation/mixins/command.py
+++ ptero_workflow/implementation/models/operation/mixins/command.py
@@ -1,48 +1,14 @@
 from ...job import Job, ResponseLink
+from .base import BasePetriMixin
 from sqlalchemy.orm.session import object_session
 import os
 import requests
 import simplejson
 
 
-class OperationPetriMixin(object):
+class OperationPetriMixin(BasePetriMixin):
     def _method_place_name(self, method, kind):
         return '%s-%s-%s' % (self.unique_name, method, kind)
-
-    def get_petri_transitions(self):
-        transitions = []
-
-        input_deps_place = self._attach_input_deps(transitions)
-
-        split_place = self._attach_split(transitions, input_deps_place)
-        action_place = self._attach_action(transitions, split_place)
-        join_place = self._attach_join(transitions, action_place)
-
-        self._attach_output_deps(transitions, join_place)
-
-        return transitions
-
-    def _attach_input_deps(self, transitions):
-        transitions.append({
-            'inputs': [o.success_place_pair_name(self) for o in self.input_ops],
-            'outputs': [self.ready_place_name],
-        })
-
-        return self.ready_place_name
-
-    def _attach_output_deps(self, transitions, internal_success_place):
-        success_outputs = [self.success_place_pair_name(o) for o in self.output_ops]
-        success_outputs.append(self.success_place_pair_name(self.parent))
-        transitions.append({
-            'inputs': [internal_success_place],
-            'outputs': success_outputs,
-        })
-
-    def _attach_split(self, transitions, ready_place):
-        return ready_place
-
-    def _attach_join(self, transitions, action_done_place):
-        return action_done_place
 
     def _attach_action(self, transitions, action_ready_place):
         input_place_name = action_ready_place
