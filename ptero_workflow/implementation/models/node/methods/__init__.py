c3d542a05a365ee23aa124a37a88512cc7535fc4
David Morton <davidlmorton@gmail.com>
2014-10-14 12:04:43 -0500

    Calculate subclass_lookup programatically

--- ptero_workflow/implementation/models/node/methods/__init__.py
+++ ptero_workflow/implementation/models/node/methods/__init__.py
@@ -1,14 +1,20 @@
 import method_base
 from .shell_command import *
 
-METHOD_SUBCLASSES = {
-        'ShellCommand': ShellCommand,
-        }
+METHOD_SUBCLASSES = [ShellCommand]
+
+def _calculate_subclass_lookup():
+    result = {}
+    for subclass in METHOD_SUBCLASSES:
+        service = subclass.__mapper_args__['polymorphic_identity']
+        result[service] = subclass
+    return result
+SUBCLASS_LOOKUP = _calculate_subclass_lookup()
 
 def new_method(**kwargs):
     service = kwargs['service']
-    if service in METHOD_SUBCLASSES:
-        return METHOD_SUBCLASSES[service](**kwargs)
+    if service in SUBCLASS_LOOKUP:
+        return SUBCLASS_LOOKUP[service](**kwargs)
     else:
         raise TypeError("Could not determine subclass from service (%s)" %
                 service)
