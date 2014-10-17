fabf74e9d7de93acc1cbe40fc6a897f5e766fea3
Mark Burnett <mark.m.burnett@gmail.com>
2014-10-17 10:31:47 -0500

    remove outputs from Workflow.as_dict

--- ptero_workflow/implementation/models/workflow.py
+++ ptero_workflow/implementation/models/workflow.py
@@ -51,16 +51,10 @@ class Workflow(Base):
                 if name not in ['input connector', 'output connector']}
         edges = [l.as_dict for l in self.edges]
 
-        try:
-            outputs = self.root_task.get_outputs(color=0)
-        except:
-            outputs = None
-
         data = {
             'tasks': tasks,
             'edges': edges,
             'inputs': self.root_task.get_inputs(colors=[0], parallel_index=0),
-            'outputs': outputs,
             'environment': simplejson.loads(self.environment),
         }
         if self.root_task.status is not None:
