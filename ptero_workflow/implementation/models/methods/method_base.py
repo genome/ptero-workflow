7978be0dfa55f019b36a627296d1c7105c727a17
Michael J. Kiwala <michael.kiwala@gmail.com>
2015-01-09 15:21:44 -0600

    document why url_for() is not being used to generate a url

--- ptero_workflow/implementation/models/methods/method_base.py
+++ ptero_workflow/implementation/models/methods/method_base.py
@@ -45,6 +45,9 @@ class Method(Base):
         else:
             query_string = ''
 
+        # We cannot use url_for() here because this is called from outside the
+        # Flask Application Context
+
         return 'http://%s:%d/v1/callbacks/methods/%d/callbacks/%s%s' % (
             os.environ.get('PTERO_WORKFLOW_HOST', 'localhost'),
             int(os.environ.get('PTERO_WORKFLOW_PORT', 80)),
