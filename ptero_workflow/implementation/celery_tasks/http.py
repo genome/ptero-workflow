61621f2313764ac658a7affa0ee13c01dd57261e
David Morton <davidlmorton@gmail.com>
2015-02-07 20:45:38 +0000

    Retry connection errors

--- ptero_workflow/implementation/celery_tasks/http.py
+++ ptero_workflow/implementation/celery_tasks/http.py
@@ -2,6 +2,7 @@ import celery
 import logging
 import requests
 import json
+from requests.exceptions import ConnectionError
 from ptero_common.logging_configuration import logged_request
 
 __all__ = ['HTTP']
@@ -14,10 +15,18 @@ class HTTP(celery.Task):
     ignore_result = True
 
     def run(self, method, url, **kwargs):
-        response = getattr(logged_request, method.lower())(url, data=self.body(kwargs),
-                headers={'Content-Type': 'application/json'}, logger=LOG)
+        try:
+            response = getattr(logged_request, method.lower())(
+                url, data=self.body(kwargs),
+                headers={'Content-Type': 'application/json'},
+                timeout=10, logger=LOG)
+        except ConnectionError as exc:
+            LOG.info("A ConnectionError occured while attempting to send %s  %s, retrying in 60 seconds.", method.upper(), url)
+            self.retry(exc=exc, countdown=60)
 
         if response.status_code >= 500:
+            LOG.info("Got response (%s), retrying after 3 minutes.",
+                    response.status_code)
             raise celery.exceptions.Retry('status_code == %s'
                     % response.status_code)
 
