1c59710c06245fe5783b27979d5d11e07a47c324
Michael J. Kiwala <michael.kiwala@gmail.com>
2015-02-22 20:52:29 -0600

    roundtrip tests deeply compares actual and expected result

--- tests/api/v1/test_roundtrip_success.py
+++ tests/api/v1/test_roundtrip_success.py
@@ -1,5 +1,8 @@
 from ..base import BaseAPITest
 import abc
+import difflib
+import json
+import sys
 
 
 class RoundTripSuccess(object):
@@ -21,10 +24,22 @@ class RoundTripSuccess(object):
 
     def test_get_should_return_post_data(self):
         get_response = self.get(self.response.headers.get('Location'))
-        key_names = ['tasks', 'links', 'inputs']
-        for key in key_names:
-            self.assertItemsEqual(self.post_data[key], get_response.DATA[key])
+        del(get_response.DATA['reports'])
+        self.assertTrue(self.compareDictAsJSON(expected=self.post_data,
+            actual=get_response.DATA))
 
+    def _to_json(self, data):
+        return json.dumps( data, indent=4, sort_keys=True, default=str )
+
+    def compareDictAsJSON(self, expected, actual):
+        is_ok = 1
+        expected_json = self._to_json(expected).splitlines(1)
+        actual_json = self._to_json(actual).splitlines(1)
+        for line in difflib.unified_diff(expected_json, actual_json,
+                fromfile='Expected', tofile='Actual'):
+            is_ok = 0
+            sys.stdout.write(line)
+        return is_ok
 
 class SingleNodeWorkflow(RoundTripSuccess, BaseAPITest):
     post_data = {
