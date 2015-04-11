2bb24c755ce4790dd530dd7e06ae62b0e4e6c0c7
David Morton <davidlmorton@gmail.com>
2015-04-11 01:46:17 +0000

    Add schema to allow more webhooks
    1) on the top level workflow
    2) on tasks
    3) on dag methods (shell-command methods already support them)

--- tests/api/v1/test_post_workflow_failure.py
+++ tests/api/v1/test_post_workflow_failure.py
@@ -19,10 +19,60 @@ class PostWorkflowFailure(object):
         self.assertEqual({'error': self.expected_error_message},
                 self.response.DATA)
 
+class BorkIsInvalidWebhookName(PostWorkflowFailure, BaseAPITest):
+    expected_error_message = "Additional properties are not allowed (u'bork' was unexpected)"
+    post_data = {
+        'webhooks': {
+            'scheduled': 'http://localhost/example/webhook',
+            'running': ['http://localhost/example/webhook', 'http://localhost/example/webhook'],
+            'failed': ['http://localhost/example/webhook'],
+            'errored': ['http://localhost/example/webhook'],
+            'succeeded': ['http://localhost/example/webhook'],
+            'failed': ['http://localhost/example/webhook'],
+            'canceled': ['http://localhost/example/webhook'],
+            'ended': ['http://localhost/example/webhook'],
+            'bork': ['http://localhost/example/webhook'],
+        },
+        'tasks': {
+            'A': {
+                'methods': [
+                    {
+                        'name': 'execute',
+                        'service': 'shell-command',
+                        'parameters': {
+                            'commandLine': ['cat'],
+                            'user': 'testuser',
+                            'workingDirectory': '/test/working/directory'
+                        }
+                    }
+                ]
+            },
+        },
+        'links': [
+            {
+                'source': 'input connector',
+                'destination': 'A',
+                'sourceProperty': 'in_a',
+                'destinationProperty': 'param',
+            }, {
+                'source': 'A',
+                'destination': 'output connector',
+                'sourceProperty': 'result',
+                'destinationProperty': 'out_a',
+            },
+        ],
+        'inputs': {
+            'in_a': 'kittens',
+        },
+    }
+
 
 class InputConnectorIsInvalidNodeName(PostWorkflowFailure, BaseAPITest):
     expected_error_message = '"input connector" is an illegal task name'
     post_data = {
+        'webhooks': {
+            'running': ['http://localhost/example/webhook', 'http://localhost/example/webhook'],
+        },
         'tasks': {
             'input connector': {
                 'methods': [
