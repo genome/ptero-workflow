821d08a98e3bbb3f35d1f528941f2cc36b5f8725
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-09-03 15:05:08 -0500

    Disable lsf system tests unless an lsf service is available

--- tests/api/v1/test_lsf_system_tests.py
+++ tests/api/v1/test_lsf_system_tests.py
@@ -10,8 +10,8 @@ MODULE = sys.modules[__name__]
 def base_dir():
     return os.path.dirname(os.path.abspath(__file__))
 
-
-generator.create_test_cases(target_module=MODULE,
+if os.environ.get('PTERO_LSF_HOST') is not None:
+    generator.create_test_cases(target_module=MODULE,
         test_case_directory=os.path.join(base_dir(),'lsf_system_tests'))
 
 
