1ae561231817a67080362742b842567c822f336a
David Morton <davidlmorton@gmail.com>
2016-01-10 08:02:30 +0000

    bugfix: use proper json capitalization scheme

--- ptero_workflow/implementation/models/execution/execution_base.py
+++ ptero_workflow/implementation/models/execution/execution_base.py
@@ -231,7 +231,7 @@ class ExecutionStatusHistory(Base):
 
     def as_dict_for_limited_report(self):
         return {
-                'execution_id': self.execution_id,
+                'executionId': self.execution_id,
                 'timestamp': str(self.timestamp),
                 'status': self.status
         }
